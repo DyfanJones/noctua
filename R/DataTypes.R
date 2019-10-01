ee588fb78d84e386e06e1f2e7965422bcd4ddde1
DyfanJones <dyfan.jones@shopdirect.com>
2019-10-01 09:45:57 +0100

    added colnames in returning vector

--- R/DataTypes.R
+++ R/DataTypes.R
@@ -17,6 +17,7 @@ AthenaDataType <-
 
 
 AthenaToRDataType <- function(data_type){
+  Names <- tolower(sapply(data_type, function(x) x$Name))
   Types <- tolower(sapply(data_type, function(x) x$Type))
   athena_to_r <- function(x){
     switch(x,
@@ -33,5 +34,7 @@ AthenaToRDataType <- function(data_type){
            date = "Date",
            timestamp = "POSIXct",
            x)}
-  vapply(Types, athena_to_r, FUN.VALUE = character(1))
+  output <- vapply(Types, athena_to_r, FUN.VALUE = character(1))
+  names(output) <- Names
+  output
 }
