b4693d97251a1e5272572b9e22a11fc8338aae69
DyfanJones <dyfan.jones@shopdirect.com>
2020-01-05 12:33:44 +0000

    changed database to schema in dbListTables

--- R/Connection.R
+++ R/Connection.R
@@ -324,7 +324,8 @@ setMethod(
 #' Returns the unquoted names of Athena tables accessible through this connection.
 #' @name dbListTables
 #' @inheritParams DBI::dbListTables
-#' @param database Athena database, default set to NULL to return all tables from all Athena databases
+#' @param schema Athena schema, default set to NULL to return all tables from all Athena schemas.
+#'               Note: The use of DATABASE and SCHEMA is interchangeable within Athena.
 #' @aliases dbListTables
 #' @return \code{dbListTables()} returns a character vector with all the tables from Athena.
 #' @seealso \code{\link[DBI]{dbListTables}}
@@ -351,11 +352,11 @@ NULL
 #' @export
 setMethod(
   "dbListTables", "AthenaConnection",
-  function(conn, database = NULL, ...){
+  function(conn, schema = NULL, ...){
     if (!dbIsValid(conn)) {stop("Connection already closed.", call. = FALSE)}
-    if(is.null(database)){
-      tryCatch(database <- sapply(conn@ptr$glue$get_databases()$DatabaseList,function(x) x$Name))}
-    tryCatch(output <- lapply(database, function (x) conn@ptr$glue$get_tables(DatabaseName = x)$TableList))
+    if(is.null(schema)){
+      tryCatch(schema <- sapply(conn@ptr$glue$get_databases()$DatabaseList,function(x) x$Name))}
+    tryCatch(output <- lapply(schema, function (x) conn@ptr$glue$get_tables(DatabaseName = x)$TableList))
     unlist(lapply(output, function(x) sapply(x, function(y) y$Name)))
   }
 )
