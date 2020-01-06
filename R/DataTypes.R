c26fe2da13352bafb7cdb3f1a13614f657252361
DyfanJones <dyfan.jones@shopdirect.com>
2020-01-06 16:58:11 +0000

    adapted AthenaToRDataType to have mutliple methods depending on file parser

--- R/DataTypes.R
+++ R/DataTypes.R
@@ -16,26 +16,55 @@ AthenaDataType <-
     )
   }
 
+# ==========================================================================
+# convert Athena types to R classes
+AthenaToRDataType <- function(method, data_type) UseMethod("AthenaToRDataType")
 
-AthenaToRDataType <- function(data_type){
-  Names <- sapply(data_type, function(x) x$Name)
-  Types <- tolower(sapply(data_type, function(x) x$Type))
-  athena_to_r <- function(x){
-    switch(x,
-           boolean = "logical",
-           int ="integer",
-           integer = "integer",
-           tinyint = "integer",
-           smallint = "integer",
-           bigint = "integer64",
-           float = "double",
-           decimal = "double",
-           string = "character",
-           varchar = "character",
-           date = "Date",
-           timestamp = "POSIXct",
-           x)}
-  output <- vapply(Types, athena_to_r, FUN.VALUE = character(1))
-  names(output) <- Names
-  output
-}
+AthenaToRDataType.athena_data.table <- 
+  function(method, data_type){
+    Names <- sapply(data_type, function(x) x$Name)
+    Types <- tolower(sapply(data_type, function(x) x$Type))
+    athena_to_r <- function(x){
+      switch(x,
+             boolean = "logical",
+             int ="integer",
+             integer = "integer",
+             tinyint = "integer",
+             smallint = "integer",
+             bigint = "integer64",
+             float = "double",
+             decimal = "double",
+             string = "character",
+             varchar = "character",
+             date = "Date",
+             timestamp = "POSIXct",
+             x)}
+    output <- vapply(Types, athena_to_r, FUN.VALUE = character(1))
+    names(output) <- Names
+    output
+  }
+
+AthenaToRDataType.athena_vroom <- 
+  function(method, data_type){
+    Names <- sapply(data_type, function(x) x$Name)
+    Types <- tolower(sapply(data_type, function(x) x$Type))
+    athena_to_r <- function(x){
+      switch(x,
+             boolean = "l",
+             int ="i",
+             integer = "i",
+             tinyint = "i",
+             smallint = "i",
+             bigint = "i64",
+             double = "d",
+             float = "d",
+             decimal = "d",
+             string = "c",
+             varchar = "c",
+             date = "D",
+             timestamp = "T",
+             x)}
+    output <- vapply(Types, athena_to_r, FUN.VALUE = character(1))
+    names(output) <- Names
+    output
+  }
