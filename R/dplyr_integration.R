60474b8e0083437efeb37035af6ee6e2c26039e7
DyfanJones <dyfan.jones@shopdirect.com>
2020-02-03 13:00:57 +0000

    db_compute now allows for creation of tables in different schema's

--- R/dplyr_integration.R
+++ R/dplyr_integration.R
@@ -66,8 +66,17 @@ db_compute.AthenaConnection <- function(con,
                                         sql,
                                         ...) {
   db_save_query <- pkg_method("db_save_query", "dplyr")
+  in_schema <- pkg_method("in_schema", "dbplyr")
+  
   table <- db_save_query(con, sql, table, ...)
-  table
+  if (grepl("\\.", table)) {
+    schema <- gsub("\\..*", "" , table)
+    table <- gsub(".*\\.", "" , table)
+  } else {
+    schema <- con@info$dbms.name
+    table <- table}
+
+  in_schema(schema, table)
 }
 
 #' S3 implementation of \code{db_save_query} for Athena
