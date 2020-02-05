8a8c806b9e67ad50a52765dddb2682ea6b009956
DyfanJones <dyfan.jones@shopdirect.com>
2020-02-05 09:43:30 +0000

    changed test function to s3_upload_location

--- tests/testthat/test-s3-upload-location.R
+++ tests/testthat/test-s3-upload-location.R
@@ -10,8 +10,8 @@ test_that("Check if the S3 upload location is correctly built",{
   partition <- c("YEAR"= 2000)
   
   # schema and name not s3 location 
-  s3_1 <- test_s3_upload_location(x, schema, name, file.type = "csv", compress = F, s3.location = s3.location, append = F)
-  s3_2 <- test_s3_upload_location(x, schema, name, file.type = "csv", compress = T, partition = partition, s3.location = s3.location, append = T)
+  s3_1 <- noctua:::s3_upload_location(x, schema, name, file.type = "csv", compress = F, s3.location = s3.location, append = F)
+  s3_2 <- noctua:::s3_upload_location(x, schema, name, file.type = "csv", compress = T, partition = partition, s3.location = s3.location, append = T)
   
   x <- c("dummy_file.csv.gz","dummy_file.csv.gz")
   schema <- "test"
@@ -20,8 +20,8 @@ test_that("Check if the S3 upload location is correctly built",{
   partition <- c("YEAR"= 2000)
   
   # schema and name in s3 location 
-  s3_3 <- test_s3_upload_location(x, schema, name, file.type = "tsv", compress = F, partition = partition, s3.location = s3.location, append = F)
-  s3_4 <- test_s3_upload_location(x, schema, name, file.type = "tsv", compress = T, partition = partition, s3.location = s3.location, append = T)
+  s3_3 <- noctua:::s3_upload_location(x, schema, name, file.type = "tsv", compress = F, partition = partition, s3.location = s3.location, append = F)
+  s3_4 <- noctua:::s3_upload_location(x, schema, name, file.type = "tsv", compress = T, partition = partition, s3.location = s3.location, append = T)
   
   x <- c("dummy_file.csv.gz")
   schema <- "test"
@@ -30,13 +30,13 @@ test_that("Check if the S3 upload location is correctly built",{
   partition <- c("YEAR"= 2000)
   
   # / at end of s3 location
-  s3_5 <- test_s3_upload_location(x, schema, name, file.type = "parquet", compress = F, s3.location = s3.location, append = F)
-  s3_6 <- test_s3_upload_location(x, schema, name, file.type = "parquet", compress = T, partition = partition, s3.location = s3.location, append = T)
+  s3_5 <- noctua:::s3_upload_location(x, schema, name, file.type = "parquet", compress = F, s3.location = s3.location, append = F)
+  s3_6 <- noctua:::s3_upload_location(x, schema, name, file.type = "parquet", compress = T, partition = partition, s3.location = s3.location, append = T)
 
-  expect_equal(s3_1, s3_loc$exp_s3_1)
-  expect_equal(s3_2, s3_loc$exp_s3_2)
-  expect_equal(s3_3, s3_loc$exp_s3_3)
-  expect_equal(s3_4, s3_loc$exp_s3_4)
-  expect_equal(s3_5, s3_loc$exp_s3_5)
-  expect_equal(s3_6, s3_loc$exp_s3_6)
+  expect_equal(s3_1[[2]], s3_loc$exp_s3_1)
+  expect_equal(s3_2[[2]], s3_loc$exp_s3_2)
+  expect_equal(s3_3[[2]], s3_loc$exp_s3_3)
+  expect_equal(s3_4[[2]], s3_loc$exp_s3_4)
+  expect_equal(s3_5[[2]], s3_loc$exp_s3_5)
+  expect_equal(s3_6[[2]], s3_loc$exp_s3_6)
 })
