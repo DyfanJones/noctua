bad9e8e7321c1390972f837bdb51fb28f619378e
DyfanJones <dyfan.jones@shopdirect.com>
2020-02-07 13:57:45 +0000

    fixed issue of partitions not returning when listing column names

--- R/dplyr_integration.R
+++ R/dplyr_integration.R
@@ -248,9 +248,13 @@ db_query_fields.AthenaConnection <- function(con, sql, ...) {
     
     tryCatch(
       output <- con@ptr$glue$get_table(DatabaseName = dbms.name,
-                                       Name = Table)$Table$StorageDescriptor$Columns)
+                                       Name = Table)$Table)
     
-    sapply(output, function(y) y$Name)
+    col_names = vapply(output$StorageDescriptor$Columns, function(y) y$Name, FUN.VALUE = character(1))
+    partitions = vapply(output$PartitionKeys,function(y) y$Name, FUN.VALUE = character(1))
+    
+    c(col_names, partitions)
+    
   } else { # If a subquery, query Athena for the fields
     # return dplyr methods
     sql_select <- pkg_method("sql_select", "dplyr")
