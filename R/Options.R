3bcb23da512ee152a61c5fdfbff7f910cfe9e8b2
DyfanJones <dyfan.jones@shopdirect.com>
2020-02-26 14:39:19 +0000

    added cache_size to documentation

--- R/Options.R
+++ R/Options.R
@@ -11,8 +11,10 @@ athena_option_env$cache_dt <-  cache_dt
 # ==========================================================================
 # Setting file parser method
 
-#' A method to change noctua backend file parser.
+#' A method to configue noctua backend options.
 #'
+#' \code{noctua_options()} provides a method to change the backend. This includes changing the file parser
+#' and whether \code{noctua} should cache query ids locally.
 #' @param file_parser Method to read and write tables to Athena, currently defaults to data.table
 #' @param cache_size Number of queries to be cached. Currently only support caching up to 50 queries.
 #' @return \code{noctua_options()} returns \code{NULL}, invisibly.
