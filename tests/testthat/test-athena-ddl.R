075346374eba79e36b4f185e4b5d05cbb69f5f61
DyfanJones <dyfan.jones@shopdirect.com>
2020-03-09 19:38:52 +0000

    added json to athena ddl unit test

--- tests/testthat/test-athena-ddl.R
+++ tests/testthat/test-athena-ddl.R
@@ -13,13 +13,29 @@ test_that("Check if Athena DDL's are created correctly",{
   # Test connection is using AWS CLI to set profile_name 
   con <- dbConnect(athena())
   
+  # CSV DDL
   expect_ddl1 <- sqlCreateTable(con, "test_df", df, s3.location = s3.location, file.type = "csv")
-  expect_ddl2 <- sqlCreateTable(con, "test_df", df, s3.location = s3.location, file.type = "tsv")
-  expect_ddl3 <- sqlCreateTable(con, "test_df", df, s3.location = s3.location, file.type = "parquet")
-  expect_ddl4 <- sqlCreateTable(con, "test_df", df, partition = "timestamp", s3.location = s3.location, file.type = "parquet")
-
+  expect_ddl2 <- sqlCreateTable(con, "test_df", df, s3.location = s3.location, file.type = "csv", compress = T)
+  
+  # TSV DDL
+  expect_ddl3 <- sqlCreateTable(con, "test_df", df, s3.location = s3.location, file.type = "tsv")
+  expect_ddl4 <- sqlCreateTable(con, "test_df", df, s3.location = s3.location, file.type = "tsv", compress = T)
+  
+  # Parquet DDL
+  expect_ddl5 <- sqlCreateTable(con, "test_df", df, s3.location = s3.location, file.type = "parquet")
+  expect_ddl6 <- sqlCreateTable(con, "test_df", df, partition = "timestamp", s3.location = s3.location, file.type = "parquet", compress = T)
+  
+  # JSON DDL
+  expect_ddl7 <- sqlCreateTable(con, "test_df", df, s3.location = s3.location, file.type = "json")
+  expect_ddl8 <- sqlCreateTable(con, "test_df", df, partition = "timestamp", s3.location = s3.location, file.type = "json", compress = T)
+  
+  
   expect_equal(expect_ddl1, tbl_ddl$tbl1)
   expect_equal(expect_ddl2, tbl_ddl$tbl2)
   expect_equal(expect_ddl3, tbl_ddl$tbl3)
   expect_equal(expect_ddl4, tbl_ddl$tbl4)
+  expect_equal(expect_ddl5, tbl_ddl$tbl5)
+  expect_equal(expect_ddl6, tbl_ddl$tbl6)
+  expect_equal(expect_ddl7, tbl_ddl$tbl7)
+  expect_equal(expect_ddl8, tbl_ddl$tbl8)
 })
