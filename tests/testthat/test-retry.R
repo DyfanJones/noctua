f7f70e870f488924e1dc01a1c64a25adb7e1d905
DyfanJones <dyfan.jones@shopdirect.com>
2020-03-19 12:31:04 +0000

    created unit test for retry_api_call

---
+++ tests/testthat/test-retry.R
@@ -0,0 +1,19 @@
+context("Testing retry function")
+
+test_that("Check if retry_api is working as intended",{
+  
+  # create a function that is designed to fail so many times
+  fail_env <- new.env()
+  fail_env$i <- 1
+  
+  fail_function <- function(i, j){
+    if (i > j) i <- 1
+    result <- (i == j)
+    fail_env$i <- i + 1
+    if(!result) stop(i, " does not equal 2")
+    return(TRUE)
+  }
+  
+  # this function will fail twice before succeeding
+  expect_true(retry_api_call(fail_function(fail_env$i , 3)))
+})
