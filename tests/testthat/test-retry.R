d7bebd689f5f2c889025c511fb7aeb077174c673
DyfanJones <dyfan.jones@shopdirect.com>
2020-04-16 12:44:14 +0100

    test noctua retry when set to 0

--- tests/testthat/test-retry.R
+++ tests/testthat/test-retry.R
@@ -16,6 +16,11 @@ test_that("Check if retry_api is working as intended",{
   
   # this function will fail twice before succeeding
   expect_true(retry_api_call(fail_function(fail_env$i , 3)))
+  
+  # stop noctua retrying and expect error
+  noctua_options(retry = 0)
+  expect_error(retry_api_call(fail_function(fail_env$i, 3)))
+  
   expect_error(noctua_options(retry = - 10))
   expect_error(noctua_options(retry_quiet = "blah"))
 })
