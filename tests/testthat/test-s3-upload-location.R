1766f6b2ee8f86f0beeef0513c56540489b21f1b
DyfanJones <dyfan.jones@shopdirect.com>
2020-10-11 09:41:53 +0100

    added expeceted s3 location build for partitions

--- tests/testthat/test-s3-upload-location.R
+++ tests/testthat/test-s3-upload-location.R
@@ -40,6 +40,7 @@ test_that("Check if the S3 upload location is correctly built",{
   s3.location <- "s3://bucket/path/to/file/dummy_table"
   name <- "schema.dummy_table"
   s3_9 <- noctua:::s3_upload_location(conn, s3.location, name, partition, TRUE)
+  s3_10 <- noctua:::s3_upload_location(conn, s3.location, name, NULL, TRUE)
   
   expect_equal(s3_1, list(Bucket = "bucket", Key = "path/to/file", Schema = "default", Name = "dummy_table", Partition = "YEAR=2000"))
   expect_equal(s3_2, list(Bucket = "bucket", Key = "path/to/file", Schema = "default", Name = "dummy_table", Partition = NULL))
@@ -49,5 +50,6 @@ test_that("Check if the S3 upload location is correctly built",{
   expect_equal(s3_6, list(Bucket = "bucket", Key = "path/schema/to/file", Schema = NULL, Name = "dummy_table", Partition = NULL))
   expect_equal(s3_7, list(Bucket = "bucket", Key = "path/to/file/schema/dummy_table", Schema = NULL, Name = NULL, Partition = "YEAR=2000"))
   expect_equal(s3_8, list(Bucket = "bucket", Key = "path/to/file/schema/dummy_table", Schema = NULL, Name = NULL, Partition = NULL))
-  expect_equal(s3_9, list(Bucket = "bucket", Key = "path/to/file/dummy_table", Schema = NULL, Name = NULL, Partition = NULL))
+  expect_equal(s3_9, list(Bucket = "bucket", Key = "path/to/file/dummy_table", Schema = NULL, Name = NULL, Partition = "YEAR=2000"))
+  expect_equal(s3_10, list(Bucket = "bucket", Key = "path/to/file/dummy_table", Schema = NULL, Name = NULL, Partition = NULL))
 })
