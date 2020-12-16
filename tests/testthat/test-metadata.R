0d16a5b554765debdb1a6c7cf32834587c2df2bc
DyfanJones <dyfan.jones@shopdirect.com>
2020-12-16 16:50:43 +0000

    tests: check if dbGetPartition new format is working correctly.

--- tests/testthat/test-metadata.R
+++ tests/testthat/test-metadata.R
@@ -24,20 +24,27 @@ test_that("Returning meta data",{
   con_info_exp = names(dbGetInfo(con))
   list_tbl1 = any(grepl("test_df", dbListTables(con, "default")))
   list_tbl2 = nrow(dbGetTables(con, "default")[TableName == "test_df"]) == 1
-  partition = grepl("timestamp", dbGetPartition(con, "test_df")[[1]])
+  partition1 = grepl("timestamp", dbGetPartition(con, "test_df")[[1]])
+  
+  partition2 = names(dbGetPartition(con, "test_df", .format = T)) == "timestamp"
+  noctua_options("vroom")
+  partition3 = names(dbGetPartition(con, "test_df", .format = T)) == "timestamp"
+  
+  noctua_options()
   db_show_ddl = gsub(", \n  'transient_lastDdlTime'.*",")", dbShow(con, "test_df"))
   db_info = dbGetInfo(con)
   
   dbClearResult(res)
   dbDisconnect(con)
   
-  
   expect_equal(column_info1, df_col_info)
   expect_equal(column_info2, col_info_exp)
   expect_equal(con_info, con_info_exp)
   expect_true(list_tbl1)
   expect_true(list_tbl2)
-  expect_true(partition)
+  expect_true(partition1)
+  expect_true(partition2)
+  expect_true(partition3)
   expect_equal(db_show_ddl, show_ddl)
   expect_warning(noctua:::time_check(Sys.time() + 10))
   expect_error(noctua:::pkg_method("made_up", "made_up_pkg"))
