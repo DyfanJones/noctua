726c6decc78d6604e80413fce36bb5cfd4bb0ea2
DyfanJones <dyfan.jones@shopdirect.com>
2021-01-29 10:18:52 +0000

    feature: improved performance for hex2raw parser. json parser now utilises chunks to help with memory limitations (#135)

--- R/column_parser.R
+++ R/column_parser.R
@@ -4,8 +4,8 @@
 
 # Takes a string and converts it to raw
 hex2raw <- function(string){
-  split_str = strsplit(string, " ")[[1]]
-  as.raw(as.hexmode(split_str))
+  split_str = strsplit(string, split = " ", fixed = TRUE)
+  return(lapply(split_str, function(x) as.raw(as.hexmode(x))))
 }
 
 # applying string convertion across entire data frame
@@ -14,12 +14,37 @@ raw_parser <- function(output, columns){
   # only convert Athena data types `varbinary`
   for (col in names(columns[columns %in% c("varbinary")])) {
     tryCatch({
-      set(output, j=col, value=lapply(output[[col]], hex2raw))
+      set(output, j=col, value=hex2raw(output[[col]]))
     },
     error = function(e){
       warning(sprintf(.error_msg, col), call. = F)
     })
   }
+}
+
+split_vec <- function(vec, len, max_len = length(vec)){
+  chunks <- seq(1, max_len, len)
+  ll <- Map(function(i) list(), 1:length(chunks))
+  for (i in seq_along(chunks))
+    ll[[i]] <- vec[chunks[i]:min(chunks[i]+(len-1), max_len)]
+  return(ll)
+}
+
+create_json_string <- function(string){paste0("[", paste(string, collapse = ","), "]")}
+
+# chunk up json strings then collapse json chunks before parsing them 
+json_chunks <- function(string, fun=jsonlite::parse_json, min_chunk = 10000L){
+  if(length(string) < min_chunk){
+    output <- fun(paste0("[", paste(string, collapse = ","), "]"))
+  } else {
+    len <- max(ceiling(length(string)/20), min_chunk)
+    split_string <- split_vec(string, len)
+    output <- unlist(
+      lapply(split_string, function(i) fun(create_json_string(i))),
+      recursive = FALSE
+    )
+  }
+  return(output)
 }
 
 # parse json string
@@ -35,11 +60,15 @@ json_parser <- function(output, columns){
   }
   
   # only convert Athena data types `array` and `json`
+  # only convert Athena data types `array` and `json`
   for (col in names(columns[columns %in% c("array", "json")])) {
     tryCatch({
-      set(output, j=col, value=lapply(output[[col]], parse_json))
+      set(output,
+          j=col,
+          value=json_chunks(output[[col]], parse_json))
     },
     error = function(e){
+      message(e)
       warning(sprintf(.error_msg, col), call. = F)
     })
   }
