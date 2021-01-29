4aa87a4ba93c25d41bfd100c9e0251c9ece98e39
DyfanJones <dyfan.jones@shopdirect.com>
2021-01-29 10:21:58 +0000

    unit test: test if json and binary parsers are working as expected.

---
+++ tests/testthat/test-file-parser.R
@@ -0,0 +1,130 @@
+context("file_parser")
+
+library(data.table)
+library(dplyr)
+
+test_data <- function(N = 10000L, seed = 142L){
+  set.seed(seed)
+  data.table(
+    id=1:N,
+    original=sapply(1:N, function(x) paste(sample(letters, sample(5:10)), collapse = ",")),
+    json = jsonlite::toJSON(iris[1,1:2]))
+}
+
+output_test_data <- function(N = 10000L, temp_file){
+  dt_list <- test_data(N)
+  dt_list[, raw := lapply(original, charToRaw)]
+  dt_list[, raw_string := sapply(raw, function(x) paste(x, collapse = " "))]
+  fwrite(dt_list[,.(id, original, raw_string, json)],
+         temp_file,
+         quote = T)
+}
+
+test_file <- tempfile()
+
+data_type <- list(list(Name = c("id", "original", "raw_string", "json"),
+                       Type = c("integer", "string", "varbinary", "json")))
+
+method_1 <- "method"
+class(method_1) <- "athena_data.table"
+
+method_2 <- "method"
+class(method_2) <- "athena_vroom"
+
+test_that("Check if json has been correctly parser under chunk method",{
+  size <- 1e5
+  output_test_data(size, test_file)
+  
+  noctua::noctua_options(json="auto")
+  noctua::noctua_options(binary="character")
+  
+  dt1 <- noctua:::athena_read.athena_data.table(
+    method_1,
+    test_file,
+    data_type)
+  
+  dt2 <- noctua:::athena_read.athena_vroom(
+    method_2,
+    test_file,
+    data_type)
+  
+  expect_equal(jsonlite::toJSON(dt1$json[[1]], auto_unbox = T), jsonlite::toJSON(iris[1,1:2]))
+  expect_equal(jsonlite::toJSON(dt2$json[[1]], auto_unbox = T), jsonlite::toJSON(iris[1,1:2]))
+})
+
+test_that("Binary and json conversion",{
+  size <- 1e1
+  output_test_data(size, test_file)
+  
+  noctua::noctua_options(json="auto")
+  noctua::noctua_options(binary="raw")
+  
+  dt1 <- noctua:::athena_read.athena_data.table(
+    method_1,
+    test_file,
+    data_type)
+  
+  dt2 <- noctua:::athena_read.athena_vroom(
+    method_2,
+    test_file,
+    data_type)
+  
+  dt1[, string := sapply(raw_string, rawToChar)]
+  
+  dt2 <- dt2 %>% mutate(string = sapply(raw_string, rawToChar))
+  
+  expect_equal(jsonlite::toJSON(dt1$json[[1]], auto_unbox = T), jsonlite::toJSON(iris[1,1:2]))
+  expect_equal(jsonlite::toJSON(dt2$json[[1]], auto_unbox = T), jsonlite::toJSON(iris[1,1:2]))
+  
+  expect_equal(dt1$original, dt1$string)
+  expect_equal(dt2$original, dt2$string)
+})
+
+
+test_that("Check in conversion is turned off",{
+  size <- 1e1
+  output_test_data(size, test_file)
+  
+  noctua::noctua_options(json="character")
+  noctua::noctua_options(binary="character")
+  
+  dt1 <- noctua:::athena_read.athena_data.table(
+    method_1,
+    test_file,
+    data_type)
+  
+  dt2 <- noctua:::athena_read.athena_vroom(
+    method_2,
+    test_file,
+    data_type)
+  
+  expect_equal(dt1$json[[1]], as.character(jsonlite::toJSON(iris[1,1:2])))
+  expect_equal(dt2$json[[1]], as.character(jsonlite::toJSON(iris[1,1:2])))
+  
+  expect_true(is.character(dt1$original))
+  expect_true(is.character(dt2$original))
+})
+
+test_that("Custom json parser",{
+  size <- 1e1
+  output_test_data(size, test_file)
+  
+  noctua::noctua_options(json=jsonlite::fromJSON)
+  noctua::noctua_options(binary="character")
+  
+  dt1 <- noctua:::athena_read.athena_data.table(
+    method_1,
+    test_file,
+    data_type)
+  
+  dt2 <- noctua:::athena_read.athena_vroom(
+    method_2,
+    test_file,
+    data_type)
+  
+  expect_equal(dt1$json[[1]], iris[1,1:2])
+  expect_equal(dt2$json[[1]], iris[1,1:2])
+})
+
+# remove temporary file
+unlink(test_file)
