925304ee20c64164c116c41dc42cc941dac3a0d3
Dyfan Jones <dyfan.jones@theverygroup.com>
2022-05-03 10:15:01 +0100

    doc: point to noctua_options to turn off clear_s3_resource

--- R/Connection.R
+++ R/Connection.R
@@ -719,7 +719,9 @@ setMethod(
 #' Send query, retrieve results and then clear result set
 #'
 #' @note If the user does not have permission to remove AWS S3 resource from AWS Athena output location, then an AWS warning will be returned.
-#'       It is better use query caching \code{\link{noctua_options}} so that the warning doesn't repeatedly show.
+#'       For example \code{AccessDenied (HTTP 403). Access Denied}.
+#'       It is better use query caching or optionally prevent clear AWS S3 resource using \code{\link{noctua_options}}
+#'       so that the warning doesn't repeatedly show.
 #' @name dbGetQuery
 #' @inheritParams DBI::dbGetQuery
 #' @param statistics If set to \code{TRUE} will print out AWS Athena statistics of query.
